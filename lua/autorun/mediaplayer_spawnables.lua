679cb5d7ba5481fdfb5fddffd627b19c751b479f
rayzox57000 <kev02071996@gmail.com>
2022-03-05 01:39:36 +0100

    ADD : All Gmod Plates to MediaPlayer Spawnable Entities [Generator]

--- lua/autorun/mediaplayer_spawnables.lua
+++ lua/autorun/mediaplayer_spawnables.lua
@@ -38,6 +38,48 @@ AddMediaPlayerModel(
 	}
 )
 
+
+local function newScreen(x,y)
+	local x_ = x == 0.25 and "025" or (x == 0.5 and "05" or (x == 0.75 and "075" or x))
+	local y_ = y == 0.25 and "025" or (y == 0.5 and "05" or (y == 0.75 and "075" or y))
+	local xt = (x < 10 and x >= 1) and string.format("0%s",x) or x
+	local yt = (y < 10 and y >= 1) and string.format("0%s",y) or y
+	local s = x <= 1 and "Petit Ecran" or (x <= 5 and "Ecran Moyen" or (x <= 8 and "Grand Ecran" or "Ecran Géant"))
+
+	AddMediaPlayerModel(
+		string.format("../spawnicons/models/hunter/plates/plate%sx%s",x_,y_),
+		string.format("%s %sx%s",s,xt,yt),
+		string.format("models/hunter/plates/plate%sx%s.mdl",x_,y_),
+		{
+			angle = Angle(0, 90, 0),
+			offset = Vector(CubePX(x), CubePY(y), 2.5),
+			width = CubeSY(y),
+			height = CubeSX(x)
+		}
+	)
+end
+
+local function Screens()
+	local i = 0.25
+	local i_i = 0.25
+	while(i <= 32) do
+		i_i = i >= 1 and (i >= 8 and 8 or 1) or 0.25
+		local j = 0.25
+		local j_i = 0.25
+		while(j <= 32) do
+			j_i = j >= 1 and (j >= 8 and 8 or 1) or 0.25
+			if(j >= i) then newScreen(i,j) end
+			j = j + j_i
+		end
+		i = i + i_i
+	end
+end
+
+Screens()
+
+
+
+
 if SERVER then
 
 	-- fix for media player owner not getting set on alternate model spawn
@@ -45,7 +87,11 @@ if SERVER then
 		if not ent.IsMediaPlayerEntity then return end
 		ent:SetCreator(ply)
 		local mp = ent:GetMediaPlayer()
-		mp:SetOwner(ply)
+		if mp.SetOwner then
+			mp:SetOwner(ply)
+		else
+			ErrorNoHalt("[MediaPlayer] ",ply," ",ent," Tried to SetOwner on "..tostring(mp).." but no .SetOwner\n")
+		end
 	end )
 
 end
